6fc942095fe827df8724db95797ca687035bd676
Lisa-Stubert <lisastubert@web.de>
2023-01-02 20:20:27 +0100

    [mod] forgot importing fetch

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,7 +1,7 @@
 import { App, ExpressReceiver, ReceiverEvent } from '@slack/bolt'
 import { APIGatewayEvent, Context } from 'aws-lambda'
 import * as dotenv from 'dotenv'
-//import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 import axios from 'axios';
 dotenv.config();
 
