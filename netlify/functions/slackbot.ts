c5d3f065b708e85b50c7ebb671b431fe6a1277ff
Lisa-Stubert <lisastubert@web.de>
2023-02-05 15:03:34 +0100

    test

--- netlify/functions/slackbot.ts
+++ netlify/functions/slackbot.ts
@@ -124,7 +124,7 @@ async function replyMessage(channelId: string, messageThreadTs: string): Promise
    }
  }
 
-app.message("Was gibt's Neues im Open Data Portal?", async ({ message }) => {
+app.message("Was gibt's Neues im Berliner Open Data Portal?", async ({ message }) => {
   await replyMessage(message.channel, message.ts);
 });
 
